6f27a696d384f08e99b7b9acd7a5136494c292ce
Sam Barlow <sambarlow@Sams-MacBook-Air.local>
2023-11-07 15:05:41 +0000

    Imporved readability for generated word and single letter guess

--- milestone_2.py
+++ milestone_2.py
@@ -3,8 +3,8 @@ import random
 word_list = ['Cherry', 'Plum', 'Strawberry', 'Kiwi', 'Apple'] #Creates a list of words
 #print(word_list)
 
-word = random.choice(word_list) #Choices a random word from word_list
-#print(word)
+word_to_be_guessed = random.choice(word_list) #Choices a random word from word_list
+#print(word_to_be_guessed)
 
 guess = input("Please enter a single letter: ") #Asks the user to enter a letter and assigns it to the guess variable 
 if len(guess) == 1 and guess.isalpha(): #Checks if guessed letter is a single character and an alphabetical letter
